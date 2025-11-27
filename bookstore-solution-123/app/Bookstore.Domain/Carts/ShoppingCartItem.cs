733e20e2fb3a29ea28427e1ce635125cfd6f4c96
NoReply Amazon <no-reply@amazon.com>
2025-11-27 10:07:02 +0000

    DB transformation: Upload transformed code

--- bookstore-solution-123/app/Bookstore.Domain/Carts/ShoppingCartItem.cs
+++ bookstore-solution-123/app/Bookstore.Domain/Carts/ShoppingCartItem.cs
@@ -1,7 +1,9 @@
-﻿using Bookstore.Domain.Books;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Bookstore.Domain.Books;
 
 namespace Bookstore.Domain.Carts
 {
+    [Table("ShoppingCartItem_mod", Schema = "database-1_dbo")]
     public class ShoppingCartItem : Entity
     {
         // An empty constructor is required by EF Core
@@ -16,14 +18,18 @@ namespace Bookstore.Domain.Carts
             WantToBuy = wantToBuy;
         }
 
+        [Column("ShoppingCartId_mod")]
         public int ShoppingCartId { get; set; }
         public ShoppingCart ShoppingCart { get; set; }
 
+        [Column("BookId_mod")]
         public int BookId { get; set; }
         public Book Book { get; set; }
 
+        [Column("Quantity_mod")]
         public int Quantity { get; set; }
 
+        [Column("WantToBuy_mod")]
         public bool WantToBuy { get; set; }
     }
 }
